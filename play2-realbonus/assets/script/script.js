3270643f9f4192605f98dc9437e5b7a1d5fffeac
Lucas P Torres de Araújo <lucast0rr3s@gmail.com>
2024-02-20 14:57:14 -0300

    Update

---
+++ play2-realbonus/assets/script/script.js
@@ -0,0 +1,11 @@
+const div = document.getElementById("text");
+
+function showBtn() {
+    div.style.display = "flex";
+}
+
+function redirect() {
+    window.location.href = "https://magodopix.bet/";
+}
+
+//setTimeout(showBtn, 3000);
